1db407b8337f6a918a5cb9d818aa872d7da50ecd
kimjouny <juny4500@naver.com>
2019-12-04 23:15:38 +0900

    추가 : express.js 세팅

---
+++ app.js
@@ -0,0 +1,25 @@
+const express=require('express')
+const createError = require('http-errors');
+const bodyParser=require('body-parser');
+const logger=require('morgan');
+const ejs = require('ejs'); 
+const app=express()
+
+app.set('views',__dirname+'/views')
+app.set('view engine','html')
+app.engine('html',ejs.renderFile)
+app.use(logger('dev'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('/public'))
+
+app.get('/',(req,res)=>{
+    res.render("main.html")
+})
+
+app.use(function(req, res, next) {
+    next(createError(404));
+});
+  
+
+app.listen(8080,()=>console.log('Server running at port 8080'))
