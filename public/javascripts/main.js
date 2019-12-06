77b2ecca7b75394104d9856a6c468f052312656d
kimjouny <juny4500@naver.com>
2019-12-06 17:52:25 +0900

    추가 : 타이머 0까지만 감소하는 로직 추가

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,12 @@
-import {getQueryString} from './utils.js'
+import {getQueryString, $} from './utils.js'
 
 // alert(decodeURI(getQueryString().name))
+
+const timeDisplay=$('.wave-text')[0]
+
+let tid;
+
+tid=setInterval(()=>{
+    +timeDisplay.innerText--;
+    if(+timeDisplay.innerText<=0){clearInterval(tid);}
+},1000)
