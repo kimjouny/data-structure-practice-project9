6b2ebe70ffea504a7d7b3a227f5d0404e543f19d
kimjouny <juny4500@naver.com>
2019-12-09 01:04:31 +0900

    추가 : 카드 뒤집기 로직 및 효과 구현

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -5,8 +5,63 @@ import {getAllCards} from './card.js'
 const timeDisplay=$('.wave-text')[0];
 
 let tid;
+let active=-1;
 
 const cards=getAllCards();
+console.log(cards)
+
+const cardObjects=$('.card');
+
+$('.card-container')[0].addEventListener('click',(e)=>{
+    if(e.target.classList[0]!=='card')return;
+    switch(e.target.classList.length){
+        case 1:{
+            e.target.classList.toggle('lock',true);
+            e.target.style.backgroundColor="gray";
+            e.target.style.backgroundImage=`url(../images/lock.png)`;
+            e.target.style.backgroundSize="5rem"
+            e.target.style.backgroundRepeat="no-repeat"
+            e.target.style.backgroundPosition="50%"
+            active=-1;
+            break;
+        }
+        case 2:{
+            if(e.target.classList[1]!=='lock')return;
+            if(active==-1){
+                e.target.classList.toggle('lock',false);
+                active=+e.target.value
+                e.target.style.backgroundColor=cards[+e.target.value].color;
+                e.target.style.backgroundImage=`url(${cards[+e.target.value].img})`;
+                e.target.style.backgroundSize="5rem"
+                e.target.style.backgroundRepeat="no-repeat"
+                e.target.style.backgroundPosition="50%"
+                return;
+            }
+            if(cards[active]===cards[+e.target.value]){
+                e.target.classList.toggle('lock',false);
+                e.target.style.backgroundColor=cards[+e.target.value].color;
+                e.target.style.backgroundImage=`url(${cards[+e.target.value].img})`;
+                e.target.style.backgroundSize="5rem"
+                e.target.style.backgroundRepeat="no-repeat"
+                e.target.style.backgroundPosition="50%"
+                cardObjects[active].classList.add('matched')
+                e.target.classList.add('matched')
+                active=-1;
+
+            }
+            else{
+                cardObjects[active].classList.toggle('lock',true);
+                cardObjects[active].style.backgroundColor="gray";
+                cardObjects[active].style.backgroundImage=`url(../images/lock.png)`;
+                cardObjects[active].style.backgroundSize="5rem"
+                cardObjects[active].style.backgroundRepeat="no-repeat"
+                cardObjects[active].style.backgroundPosition="50%"
+                active=-1;
+            }
+            break;
+        }
+    }
+});
 
 tid=setInterval(()=>{
     +timeDisplay.innerText--;
